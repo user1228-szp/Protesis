ecf136c0e40cc07800e9888837ae194941a9004d
Sebastian Eugenio <csebaseugenio@outlook.es>
2024-03-02 00:01:42 -0600

    FEAT_PLOT

--- all_plots.py
+++ all_plots.py
@@ -2,27 +2,25 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import time
 
-
 df = pd.read_excel('Prueba_1.xlsx')
 
-time = df.iloc[:, 0]
+time_data = df.iloc[:, 0]  
 
-relaxed_hand = df.iloc[:, 1]
-fist = df.iloc[:, 2]
+unmarked_data = df.iloc[:, 1]
+hand_at_rest = df.iloc[:, 2]
+hand_clenched_in_fist = df.iloc[:, 3]
+wrist_flexion = df.iloc[:, 4]
+wrist_extension = df.iloc[:, 5]
+radial_deviations = df.iloc[:, 6]
+ulnar_deviations = df.iloc[:, 7]
+extended_palm = df.iloc[:, 8]
 
-wrist_extension = df.iloc[:, 4]
-radius_deviation = df.iloc[:, 5]
-
-
-ulnar_deviation = df.iloc[:, 8]
-
-y_data = [relaxed_hand, fist, wrist_extension, radius_deviation, ulnar_deviation]
-
-plot_titles = ['Relaxed Hand', 'Fist', 'Wrist Extension', 'Radius Deviation', 'Ulnar Deviation']
+y_data = [unmarked_data, hand_at_rest, wrist_extension, radial_deviations, ulnar_deviations] 
+plot_titles = ['Relaxed Hand', 'Fist', 'Wrist Extension', 'Radial Deviation', 'Ulnar Deviation'] 
 plt.figure(figsize=(8, 6))
-for i, data in enumerate(y_data[:3], start=1):
+for i, data in enumerate(y_data[:4], start=1):
     plt.subplot(3, 1, i)
-    plt.plot(time, data)
+    plt.plot(time_data, data) 
     plt.title(f'Time vs {plot_titles[i-1]}')
     plt.xlabel('Time')
     plt.ylabel('Y-axis')
@@ -33,9 +31,9 @@ plt.show()
 time.sleep(2)
 
 plt.figure(figsize=(8, 6))
-for i, data in enumerate(y_data[3:], start=1):
+for i, data in enumerate(y_data[4:], start=1):
     plt.subplot(3, 1, i)
-    plt.plot(time, data)
+    plt.plot(time_data, data)
     plt.title(f'Time vs {plot_titles[i+2]}')
     plt.xlabel('Time')
     plt.ylabel('Y-axis')
